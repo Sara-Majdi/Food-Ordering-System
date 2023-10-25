a6a92f72efe54d2a4fda7d0478460d634aa7f682
Vishvaa08 <86644867+Vishvaa08@users.noreply.github.com>
2023-10-25 20:11:14 +0800

    Add Credit
    UI and retrieves info from firebase, button not yet functional

---
+++ java/addCreditAdapter.java
@@ -0,0 +1,61 @@
+package com.example.foodorderingsystem;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class addCreditAdapter extends FirebaseRecyclerAdapter<addCreditHelperClass,addCreditAdapter.viewHolder>{
+
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public addCreditAdapter(@NonNull FirebaseRecyclerOptions<addCreditHelperClass> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull viewHolder holder, int position, @NonNull addCreditHelperClass model) {
+        holder.userFullName.setText(model.getFirst_name());
+        holder.userCurrentPoints.setText(model.getPoints());
+
+        Glide.with(holder.profilePic.getContext())
+                .load(model.getUser_picture())
+                .error(R.drawable.img)
+                .into(holder.profilePic);
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_credit_view, parent, false);
+        return new viewHolder(view);
+    }
+
+    class viewHolder extends RecyclerView.ViewHolder{
+
+        ImageView profilePic;
+        TextView userFullName, userCurrentPoints;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            profilePic = (ImageView) itemView.findViewById(R.id.profilePic);
+            userFullName = (TextView) itemView.findViewById(R.id.userFullName);
+            userCurrentPoints = (TextView) itemView.findViewById(R.id.userCurrentPoints);
+        }
+    }
+
+}
