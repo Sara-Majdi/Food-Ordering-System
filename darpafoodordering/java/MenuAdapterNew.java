42983a3b4d8c973d1d14d080f14abd60e6538e34
Sara Majdi <123373205+Sara-Majdi@users.noreply.github.com>
2023-11-04 19:04:25 +0800

    Add MenuAdapter and MenuHelperClass page

---
+++ darpafoodordering/java/MenuAdapterNew.java
@@ -0,0 +1,63 @@
+package com.example.darpafoodordering;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MenuAdapterNew extends FirebaseRecyclerAdapter<MenuHelperClass,MenuAdapterNew.viewHolder>{
+
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public MenuAdapterNew(@NonNull FirebaseRecyclerOptions<MenuHelperClass> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull MenuAdapterNew.viewHolder holder, int position, @NonNull MenuHelperClass model) {
+        holder.name.setText(model.getName());
+        holder.price.setText(model.getPrice());
+
+        Glide.with(holder.image.getContext())
+                .load(model.getImage())
+                .error(R.drawable.foodhome)
+                .into(holder.image);
+    }
+
+
+
+    @NonNull
+    @Override
+    public MenuAdapterNew.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.menu_layout, parent, false);
+        return new MenuAdapterNew.viewHolder(view);
+    }
+
+    class viewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView name, price;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = (ImageView) itemView.findViewById(R.id.itemImage);
+            name = (TextView) itemView.findViewById(R.id.itemName);
+            price = (TextView) itemView.findViewById(R.id.itemPrice);
+        }
+    }
+
+}
